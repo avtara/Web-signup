7de1e07d6b074fd3fe432107b83285407a5822da
avtara <avtarakhrisna1@gmail.com>
2017-10-17 20:16:00 +0700

    Update v.1

---
+++ hasil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="atas">
+	<p>Instagram</p>
+</div>
+<div id="hasi" align="center">
+	<p style="margin-top: 10%;color: #999999">Selamat datang, <h7 style="font-weight: bold; color: #999999"><?php echo $_GET["firstname"]; ?></h7> di Instagram </p><p style="color: #999999">username anda adalah <h7 style="font-weight: bold;"><?php echo $_GET["username"]; ?></h7>
+	<br>Password anda adalah <h7 style="font-weight: bold;"><?php echo $_GET["email"]; ?></h7>
+	<br>Password anda adalah <h7 style="font-weight: bold;"><?php echo $_GET["password"]; ?></h7></p>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="atas">
+	<p>Instagram</p>
+</div>
+<div id="kiri">
+	<div class="img"><img src="image/ig.jpg"></div>
+</div>
+<div id="kanan">
+	<div class="kotak-atas">
+		<div class="gambar"><img src="image/instagram-logo.png" height="100px" width="200"></div>
+		<div class="paragraf" align="center"><p>Sign up to see photos and videos from your friends.</p></div>
+		<a href="#" class="facebook" a>Facebook</a>
+		<p class="paragraf">Or</p>
+		<div class="form">
+			<form action="hasil.php" menthod="GET">
+    			<input type="text" id="fname" name="firstname" placeholder="Name" style="width: 219px; height: 30px; border-radius:5px;border: 2px solid #e7e7e7 ; margin-left: 17%; margin-top: -10px;">
+    			<input type="text" id="uname" name="username" placeholder="Username" style="width: 219px; height: 30px; border-radius:5px;border: 2px solid #e7e7e7 ; margin-left: 17%;     margin-top: 5px;">
+    			<input type="email" id="email" name="email" placeholder="Email" required="email" style="width: 219px; height: 30px; border-radius:5px;border: 2px solid #e7e7e7 ; margin-left: 17%;     margin-top: 5px;">
+    			<input type="password" id="awd" name="password" placeholder="Password" required="password" style="width: 219px; height: 30px; border-radius:5px;border: 2px solid #e7e7e7 ; margin-left: 17%;     margin-top: 5px;";>
+    			<input class="facebook" type="submit" value="Submit" style="width: 219px; height: 30px; border-radius:5px; background: #3897f0; border: solid 1px white; margin-left: 17%;    margin-top: 15px; height: 36px">
+  			</form>
+		</div>
+		<div class="paragraf"></div>
+	</div>
+	<div class="kotak-bawah">
+		<div class="paragraf"></div>
+	</div>
+</div>
+</body>
+</html>
